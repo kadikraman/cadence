b86c0f8e6d64c1c2a08accde600c8e3211902601
Kadi Kraman <hellokadi@gmail.com>
2025-10-18 11:05:08 +0100

    Add app icon; setup prettier

---
+++ app.config.ts
@@ -0,0 +1,51 @@
+import { ExpoConfig } from "expo/config";
+
+const config: ExpoConfig = {
+  name: "cadence",
+  slug: "cadence",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  scheme: "cadence",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    icon: "./assets/cadence.icon",
+  },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    edgeToEdgeEnabled: true,
+    predictiveBackGestureEnabled: false,
+  },
+  web: {
+    output: "static",
+    favicon: "./assets/images/favicon.png",
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/icon-transparent.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#0F1419",
+        dark: {
+          backgroundColor: "#000000",
+        },
+      },
+    ],
+  ],
+  experiments: {
+    typedRoutes: true,
+    reactCompiler: true,
+  },
+};
+
+export default config;
