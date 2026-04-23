b16b29611a1dafd15c1d443807802d079d839f35
Kadi Kraman <kadi@expo.dev>
2026-04-23 22:07:33 +0100

    Switch to a monorepo

---
+++ apps/cadence/metro.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
+
+const config = getSentryExpoConfig(projectRoot);
+
+config.watchFolders = [...(config.watchFolders ?? []), workspaceRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+
+module.exports = config;
